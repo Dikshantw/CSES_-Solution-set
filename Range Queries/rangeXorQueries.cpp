5973763d09a89c9b25f99b59b8ca03f30a227794
Dikshant Wavhule <dikshantw11@gmail.com>
2026-01-01 15:17:22 +0000

    Range Queries

---
+++ Range Queries/rangeXorQueries.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n,q;
+  cin >> n >> q;
+  vector<int> prefix(n);
+  cin >> prefix[0];
+  for(int i=1; i<n; i++){
+    int x;
+    cin >> x;
+    prefix[i] = prefix[i-1] ^ x;
+  }
+
+  for(int i=0; i<q; i++){
+    int l,r;
+    cin >> l >> r;
+
+    if(l == 1){
+      cout << prefix[r-1] << '\n';
+    }else{
+      cout << (prefix[r-1] ^ prefix[l-2]) << '\n';
+    }
+  }
+  return 0;
+}
